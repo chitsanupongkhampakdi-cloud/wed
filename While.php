014dfb177038cd73b57b243158e5ce07c76eeddd
USER922-01 <user@USER922-01.local>
2026-08-13 09:37:14 +0700

    gay

---
+++ While.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="th">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>While Loop</title>
+
+    <style>
+        *{
+            margin:0;
+            padding:0;
+            box-sizing:border-box;
+            font-family:'Segoe UI',sans-serif;
+        }
+
+        body{
+            background: #33FFCC;
+            min-height:100vh;
+            display:flex;
+            justify-content:center;
+            align-items:center;
+            padding:20px;
+        }
+
+        .container{
+            width:500px;
+            background:#fff;
+            padding:30px;
+            border-radius:20px;
+            box-shadow:0 10px 25px rgba(0,0,0,0.2);
+            text-align:center;
+        }
+
+        h1{
+            color:#16a34a;
+            margin-bottom:15px;
+        }
+
+        .home-btn{
+            display:inline-block;
+            text-decoration:none;
+            background:#16a34a;
+            color:white;
---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="th">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>สูตรคูณ</title>
+
+    <style>
+        *{
+            margin:0;
+            padding:0;
+            box-sizing:border-box;
+            font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+        }
+
+        body{
+            background: #6633CC;
+            min-height:100vh;
+            display:flex;
+            justify-content:center;
+            align-items:center;
+            padding:20px;
+        }
+
+        .container{
+            background:white;
+            width:500px;
+            padding:30px;
+            border-radius:20px;
+            box-shadow:0 10px 30px rgba(0,0,0,0.2);
+            text-align:center;
+        }
+
+        h1{
+            color:#ff4d4d;
+            margin-bottom:15px;
+        }
+
+        a{
+            display:inline-block;
+            margin-bottom:20px;
+            text-decoration:none;
+        }
+    </style>
+</head>
+<body>
+
+</body>
+</html>
